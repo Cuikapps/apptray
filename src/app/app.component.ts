34dcde9f33cdf649c83d33e041cb92c4db66dce1
Amir-Afkhami <70760163+Amir-Afkhami@users.noreply.github.com>
2021-04-29 18:04:44 -0500

    app created

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cuik-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'apptray';
+
+  
+}
